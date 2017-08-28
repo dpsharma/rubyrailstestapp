86025a3a5308f792f4ed9a1d32ebb12f9733bb37
Deepak SHarma <deepak.kumar@adi-mps.com>
2017-08-28 17:35:55 +0530

    practice: model layer

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'pages/about', to: 'pages#about'
   get 'pages/services', to: 'pages#services'
   
+  # gave us a new article path, a post to create article, patch to update articles, delete articles path, show article path, index article path
+  # all of paths it gave me by simply adding resource :articles
   resources :articles
   
   root 'welcome#index'
